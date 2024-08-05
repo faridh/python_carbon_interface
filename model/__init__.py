2e48a58afa96a5634c9b9b41424fa01bfd834e69
faridh <faridh.mendoza@gmail.com>
2024-08-05 15:46:43 +0200

    Fixed/updated __init__.py files with new shipping modules

--- model/__init__.py
+++ model/__init__.py
@@ -4,5 +4,7 @@ Module model
 
 from .cabin_class import CabinClass
 from .country import Country
-from .electricity_unit import ElectricityUnit
 from .distance_unit import DistanceUnit
+from .electricity_unit import ElectricityUnit
+from .transport_method import TransportMethod
+from .weight_unit import WeightUnit
