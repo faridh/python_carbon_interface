329a022874cbdf68a680e245d482557ae07eb136
faridh <faridh.mendoza@gmail.com>
2024-08-07 10:48:26 +0200

    Added UTs to vehicles module

--- tests/test_vehicles.py
+++ tests/test_vehicles.py
@@ -4,15 +4,15 @@ Module test_vehicles
 import os
 from unittest import TestCase, mock
 
-from vehicles import VehicleMake, Vehicles
+from vehicles import VehicleMake, VehicleModel, Vehicles
 
 
+@mock.patch.dict(os.environ, {"CARBON_INTERFACE_API_KEY": "someApiKey"})
 class TestVehicles(TestCase):
     """
     UTs for vehicles module.
     """
 
-    @mock.patch.dict(os.environ, {"CARBON_INTERFACE_API_KEY": "someApiKey"})
     @mock.patch("vehicles.vehicles.Client.get")
     def test_should_get_vehicle_makes_successfully(self, mock_client) -> None:
         """
@@ -32,12 +32,34 @@ class TestVehicles(TestCase):
         self.assertEqual(response_object.name, "Alfa Romeo")
         self.assertEqual(response_object.number_of_models, 69)
 
-    @mock.patch.dict(os.environ, {"CARBON_INTERFACE_API_KEY": "someApiKey"})
     @mock.patch("vehicles.vehicles.Client.get")
-    def test_should_raise_error_when_malformed_response(self, mock_client) -> None:
+    def test_should_raise_error_when_vehicle_makes_error(self, mock_client) -> None:
         """
         Tests that the `vehicles` module raises an exception when it can't parse
         the response from the `/vehicle_makes` endpoint.
         """
         mock_client.return_value = "not json data"
         self.assertRaises(RuntimeError, Vehicles.get_vehicle_makes)
+
+    @mock.patch("vehicles.vehicles.Client.get")
+    def test_should_get_vehicle_models_successfully(self, mock_client) -> None:
+        mock_client.return_value = """
+        [{"data":{"id":"00000000-0000-0000-0000-000000000000",
+        "attributes":{"name":"Taurus","year":1993,"vehicle_make":"Ford"}}}]
+        """
+        subject: list[VehicleModel] = Vehicles.get_vehicle_models("vehicle_make_id")
+        self.assertEqual(len(subject), 1)
+        response_object: VehicleModel = subject[0]
+        self.assertEqual(response_object.id, "00000000-0000-0000-0000-000000000000")
+        self.assertEqual(response_object.name, "Taurus")
+        self.assertEqual(response_object.year, 1993)
+        self.assertEqual(response_object.vehicle_make, "Ford")
+
+    @mock.patch("vehicles.vehicles.Client.get")
+    def test_should_raise_error_when_vehicle_models_error(self, mock_client) -> None:
+        """
+        Tests that the `vehicles` module raises an exception when it can't parse
+        the response from the `/vehicle_makes` endpoint.
+        """
+        mock_client.return_value = "not json data"
+        self.assertRaises(RuntimeError, lambda: Vehicles.get_vehicle_models("id"))
