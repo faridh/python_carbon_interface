e1c5b77d802d05b4aec44bab61e366c874aede4e
faridh <faridh.mendoza@gmail.com>
2024-08-07 16:22:03 +0200

    Fixes copy-pasted docs in UTs

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,12 +5,14 @@ Module test_client
 import os
 from unittest import TestCase, mock
 
+from requests import Response
+
 from client import Client
 
 
 class TestClient(TestCase):
     """
-    Defines UTs for client module
+    UTs for client module
     """
 
     @mock.patch.dict(os.environ, {"CARBON_INTERFACE_API_KEY": "someApiKey"})
@@ -28,3 +30,27 @@ class TestClient(TestCase):
         is not present.
         """
         self.assertRaises(RuntimeError, Client)
+
+    @mock.patch.dict(os.environ, {"CARBON_INTERFACE_API_KEY": "someApiKey"})
+    @mock.patch("client.client.requests.get")
+    def test_should_execute_get_successfully(self, mock_requests) -> None:
+        """
+        Tests that client invokes `get` successfully.
+        """
+
+        mock_requests.return_value.text = '{"k": "v"}'
+        subject: Client = Client()
+        result: str = subject.get("/endpoint")
+        self.assertEqual(result, '{"k": "v"}')
+
+    @mock.patch.dict(os.environ, {"CARBON_INTERFACE_API_KEY": "someApiKey"})
+    @mock.patch("client.client.requests.post")
+    def test_should_execute_post_successfully(self, mock_requests) -> None:
+        """
+        Tests that client invokes `post` successfully.
+        """
+
+        mock_requests.return_value.text = '{"k": "v"}'
+        subject: Client = Client()
+        result: str = subject.post("/endpoint", {})
+        self.assertEqual(result, '{"k": "v"}')
